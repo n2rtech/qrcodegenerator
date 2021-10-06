eb406558ddb76b59b49095842ea9ca7921bc7c09
mohddanish <mohddanishking@gmail.com>
2021-10-06 20:11:58 +0530

    result page update

--- resources/views/frontend/results.blade.php
+++ resources/views/frontend/results.blade.php
@@ -1,9 +1,56 @@
 @extends('frontend.app')
 @section('title', 'Welcome')
 @section('content')
-    <div class="banner">
-        <div class="container">
 
+        <div class="container">
+            <div class="power-tech">
+                <div class="border-head">
+                    <div class="lebal-head">
+                        <img src="../assets/images/logo.jpg" class="img-responsive" alt="Logo">
+                            <p>D-6 Kavi Nagar Industrial Area,<br>Ghaziabad 201002- Delhi NCR</p>
+                    </div>
+                </div>
+                <div class="table-responsive border-slip">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <th>PRODUCT F.G/UID NO.</th>
+                                <th>HT Panel</th> 
+                            </tr>
+                            <tr>
+                                <th>PANEL NO.</th>
+                                <th>12</th> 
+                            </tr>
+                            <tr>
+                                <th>S.O. NO.</th>
+                                <th>34569812987</th> 
+                            </tr>
+                            <tr>
+                                <th>PANEL NAME</th>
+                                <th>11KV HT Switchboard</th> 
+                            </tr>
+                            <tr>
+                                <th>TYPE OF PANEL</th>
+                                <th>Indoor</th> 
+                            </tr>
+                            <tr>
+                                <th>Rating</th>
+                                <th>2500A</th> 
+                            </tr>
+                            <tr>
+                                <th>Files</th>
+                                <th><a href="#">Test Document.pdf</a></th> 
+                            </tr>
+                            <tr>
+                                <th>Customer: M/s XYZ <br>Electricals Private Limited</th>
+                                <th><a href="#" class="btn btn-danger">DOWNLOAD PDF</a></th> 
+                            </tr>
+                        </tbody>
+                    </table>
+                </div> 
+                    
+        </div>    
         </div>
-    </div>
+   
+
 @endsection
