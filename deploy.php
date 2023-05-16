14cc14b2e9f30d3ade2cdc77d68aa2ad56b6c60f
kulwinder <skulwinder@n2rtechnologies.in>
2023-05-16 21:41:46 +0530

    update deploy

--- deploy.php
+++ deploy.php
@@ -2,7 +2,6 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'contrib/npm.php';
 
 // Config
 
@@ -13,30 +12,11 @@ add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Hosts
-host('portfolio')
-    ->set('hostname','216.158.239.213' )
-    ->set('branch', 'master')
+
+host('216.158.239.213')
     ->set('remote_user', 'root')
     ->set('deploy_path', '/var/www/qrcodegenerator')
     ->set ('ssh_multiplexing', false);
-
-task('deploy', [
-    'deploy:prepare',
-    'deploy:vendors',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    'artisan:migrate',
-    // 'npm:install',
-    'npm:run:prod',
-    'deploy:publish',
-]);
-
-task('npm:run:prod', function () {
-    cd('{{release_or_current_path}}');
-    run('composer install');
-    // run('npm run build');
-});
 
 // Hooks
 
